c2a1865b4db9a6b99d00c8ca417818e3859a5534
WMach <wojciech.mach92@gmail.com>
2022-02-21 16:06:32 +0100

    version 1.2.1 added Words.txt to the repo, without it the code wouldn't compile before

--- Init.cs
+++ Init.cs
@@ -73,7 +73,7 @@ namespace MemoryGameObj
                     System.TimeSpan diff1 = date2.Subtract(date1);
                     Console.WriteLine("You had "+playObject.GuessesLeft+" chances left and you spent " + diff1.Seconds + " seconds on the game");
                     Console.WriteLine("Please write your name");
-                    string name=Console.ReadLine();
+                    string name=Console.ReadLine().ToString();
                     System.IO.File.WriteAllText("scores.txt", name+"|"+DateTime.Now.ToString()+"|"+ diff1.Seconds.ToString()+"|"+ playObject.GuessesLeft.ToString());
                     break;
                 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 MemoryGame2
+Why do I code? 
+It can be fun, challenging, and I can learn a lot and improve my skills(there's a reason why this repo is called MemoryGame2).
+A good brain excercise too, with all the logical thinking required.
+It's not a professional matter yet, hopefully it becomes one though.
